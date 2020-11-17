32fb90780ca4d77a66fb98c4a4a072bdaf0126f4
Jan André Reuter <jan.andre.reuter@hotmail.de>
2020-11-17 14:42:06 +0100

    Added tests for t_tra and t_max. Masks missing
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,6 +1,6 @@
 import numpy
 from matplotlib import pyplot as plt
-from scipy.signal import savgol_filter, convolve
+
 
 class TestMaskGeneration:
     def test_set_modalities(self, maskgeneration):
@@ -25,7 +25,12 @@ class TestMaskGeneration:
         assert numpy.isclose(maskgeneration.t_ret, 0.039, atol=0.001)
 
     def test_t_tra(self, maskgeneration):
-        assert False
+        retardation = numpy.zeros((100, 100))
+        retardation[10:20, 10:15] = 1
+        transmittance = numpy.random.rand(100, 100)
+
+        maskgeneration.set_modalities(transmittance, retardation)
+        assert maskgeneration.t_tra == maskgeneration.t_min
 
     def test_t_min(self, maskgeneration):
         retardation = numpy.zeros((100, 100))
@@ -35,8 +40,18 @@ class TestMaskGeneration:
         maskgeneration.set_modalities(transmittance, retardation)
         assert maskgeneration.t_min == transmittance[10:20, 10:15].mean()
 
-    def test_t_max(self):
-        assert False
+    def test_t_max(self, maskgeneration):
+        def f(x):
+            return 1000*numpy.exp(-x + 5) + 1
+        x = numpy.arange(0, 128, step=0.01)
+        y = numpy.round(f(x)).astype(int)[::-1]
+
+        image = numpy.random.choice(numpy.repeat(x, y), y.sum(),
+                                    False, numpy.ones(y.sum()) / y.sum())
+        image = image / image.max()
+        maskgeneration.transmittance = image
+
+        assert numpy.isclose(maskgeneration.t_max, 0.957, atol=0.001)
 
     def test_gray_mask(self):
         assert False
