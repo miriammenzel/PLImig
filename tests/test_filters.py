d14ed892e0a13a0987bbba8addf539c47bfb65cf
Jan André Reuter <jan.andre.reuter@hotmail.de>
2020-11-19 12:13:38 +0100

    Removed filter option as it is erronous
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,5 +1,4 @@
 import numpy
-import pytest
 from plimg import filters
 
 
@@ -9,6 +8,3 @@ class TestFilters:
         result_arr = filters.median(test_arr, kernel_size=1)
         expected_err = numpy.array([1, 1, 3, 3, 3, 3, 2, 2, 3]).reshape((3, 3))
         assert numpy.all(expected_err == result_arr)
-
-    def test_median_filter_with_mask(self):
-        pass
