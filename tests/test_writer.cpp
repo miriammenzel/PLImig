df760e705a260228dece2ea65b0063efd0b27fa2
Jan André Reuter <jan.andre.reuter@hotmail.de>
2020-11-30 11:27:36 +0100

    Added test for writing datasets
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- tests/test_writer.cpp
+++ tests/test_writer.cpp
@@ -3,8 +3,10 @@
 //
 
 #include "gtest/gtest.h"
-#include "writer.h"
 #include "H5Cpp.h"
+#include <opencv2/core.hpp>
+#include "writer.h"
+
 
 TEST(WriterTest, TestEmpty) {
     PLImg::HDF5Writer writer;
@@ -48,6 +50,37 @@ TEST(WriterTest, TestWriteAttributes) {
     attr.read(datatype, &rmax);
     attr.close();
     ASSERT_FLOAT_EQ(rmax, max);
+}
+
+TEST(WriterTest, TestWriteDataset) {
+    PLImg::HDF5Writer writer;
+
+    cv::Mat testMat(10, 10, CV_32FC1);
+    for(unsigned i = 0; i < testMat.rows; ++i) {
+        for(unsigned j = 0; j < testMat.cols; ++j) {
+            testMat.at<float>(i, j) = i * testMat.cols + j;
+        }
+    }
+
+    writer.set_path("output/writer_test_2.h5");
+    writer.write_dataset("test_write_dataset", testMat);
+    writer.close();
+
+    H5::H5File file("output/writer_test_2.h5", H5F_ACC_RDONLY);
+    H5::DataSet dataset = file.openDataSet("test_write_dataset");
+    H5::DataSpace space = dataset.getSpace();
+    H5::DataType type = dataset.getDataType();
+    hsize_t dims[2];
+    space.getSimpleExtentDims(dims);
+
+    cv::Mat readMat(dims[0], dims[1], CV_32FC1);
+    dataset.read(readMat.data, type, space);
+
+    for(unsigned i = 0; i < testMat.rows; ++i) {
+        for(unsigned j = 0; j < testMat.cols; ++j) {
+            ASSERT_FLOAT_EQ(testMat.at<float>(i, j), readMat.at<float>(i, j));
+        }
+    }
 }
 
 int main(int argc, char** argv) {
