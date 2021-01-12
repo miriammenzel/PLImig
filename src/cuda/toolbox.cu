84868b3781d79d6ac3381d5488c759908ceae20d
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-01-12 15:46:33 +0100

    Fixed a bug in both median filters resulting in black lines across the image when chunks were used.
    Fixed a bug with the chunk calculation where more than 4 chunks would result in partially black images due to wrong partitioning.
    
    Simplified code of both median filters in toolbox.cu
    Changed median filter to use pixels from nearby chunks when not at the image borders. This should give more accurate results.
    
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- src/cuda/toolbox.cu
+++ src/cuda/toolbox.cu
@@ -36,23 +36,18 @@ __device__ void shellSort(float* array, uint low, uint high) {
     }
 }
 
-__global__ void medianFilterKernel(const float* image, int image_stride, int2 image_offset,
-                                   float* result_image, int result_image_stride, int2 result_offset,
-                                   int2 roi, int2 anchor) {
-    uint thread_x = blockIdx.x * blockDim.x + threadIdx.x;
-    uint thread_y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    uint x = thread_x - anchor.x + image_offset.x;
-    uint y = thread_y - anchor.y + image_offset.y;
-    uint rx = thread_x - anchor.x + result_offset.x;
-    uint ry = thread_y - anchor.y + result_offset.y;
+__global__ void medianFilterKernel(const float* image, int image_stride,
+                                   float* result_image, int result_image_stride,
+                                   int2 imageDims) {
+    uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    uint y = blockIdx.y * blockDim.y + threadIdx.y;
 
     uint validValues = 0;
     int cy_bound;
 
     float buffer[4 * MEDIAN_KERNEL_SIZE * MEDIAN_KERNEL_SIZE];
 
-    if(x > MEDIAN_KERNEL_SIZE && x < roi.x && y > MEDIAN_KERNEL_SIZE && y < roi.y) {
+    if(x >= MEDIAN_KERNEL_SIZE && x < imageDims.x - MEDIAN_KERNEL_SIZE && y >= MEDIAN_KERNEL_SIZE && y < imageDims.y - MEDIAN_KERNEL_SIZE) {
         // Transfer image pixels to our kernel for median filtering application
         for (int cx = -MEDIAN_KERNEL_SIZE; cx <= MEDIAN_KERNEL_SIZE; ++cx) {
             cy_bound = sqrtf(MEDIAN_KERNEL_SIZE * MEDIAN_KERNEL_SIZE - cx * cx);
@@ -62,39 +57,31 @@ __global__ void medianFilterKernel(const float* image, int image_stride, int2 im
             }
         }
         shellSort(buffer, 0, validValues);
-        result_image[rx + ry * result_image_stride] = buffer[validValues / 2];
+        result_image[x + y * result_image_stride] = buffer[validValues / 2];
     } else {
-        result_image[rx + ry * result_image_stride] = 0;
+        result_image[x + y * result_image_stride] = 0.0f;
     }
-    //printf("Valid values: %d\n", validValues);
 }
 
-__global__ void medianFilterMaskedKernel(const float* image, int image_stride, int2 image_offset,
-                                         float* result_image, int result_image_stride, int2 result_offset,
-                                         const uchar* mask, int mask_stride, int2 mask_offset,
-                                         int2 roi, int2 anchor) {
-    uint thread_x = blockIdx.x * blockDim.x + threadIdx.x;
-    uint thread_y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    uint x = thread_x - anchor.x + image_offset.x;
-    uint y = thread_y - anchor.y + image_offset.y;
-    uint rx = thread_x - anchor.x + result_offset.x;
-    uint ry = thread_y - anchor.y + result_offset.y;
-    uint mx = thread_x - anchor.x + mask_offset.x;
-    uint my = thread_y - anchor.y + mask_offset.y;
+__global__ void medianFilterMaskedKernel(const float* image, int image_stride,
+                                         float* result_image, int result_image_stride,
+                                         const uchar* mask, int mask_stride,
+                                         int2 imageDims) {
+    uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    uint y = blockIdx.y * blockDim.y + threadIdx.y;
 
     uint validValues = 0;
     int cy_bound;
 
     float buffer[4 * MEDIAN_KERNEL_SIZE * MEDIAN_KERNEL_SIZE];
 
-    if(x > MEDIAN_KERNEL_SIZE && x < roi.x && y > MEDIAN_KERNEL_SIZE && y < roi.y) {
-        if(mask[mx + my * mask_stride]) {
+    if(x >= MEDIAN_KERNEL_SIZE && x < imageDims.x - MEDIAN_KERNEL_SIZE && y >= MEDIAN_KERNEL_SIZE && y < imageDims.y - MEDIAN_KERNEL_SIZE) {
+        if(mask[x + y * mask_stride]) {
             // Transfer image pixels to our kernel for median filtering application
             for (int cx = -MEDIAN_KERNEL_SIZE; cx < MEDIAN_KERNEL_SIZE; ++cx) {
                 cy_bound = sqrtf(MEDIAN_KERNEL_SIZE * MEDIAN_KERNEL_SIZE - cx * cx);
                 for (int cy = -cy_bound; cy < cy_bound; ++cy) {
-                    if (mask[mx + cx + (my + cy) * mask_stride] != 0) {
+                    if (mask[x + cx + (y + cy) * mask_stride] != 0) {
                         buffer[validValues] = image[x + cx + (y + cy) * image_stride];
                         ++validValues;
                     }
@@ -102,14 +89,14 @@ __global__ void medianFilterMaskedKernel(const float* image, int image_stride, i
             }
             if (validValues > 1) {
                 shellSort(buffer, 0, validValues);
-                result_image[rx + ry * result_image_stride] = buffer[validValues / 2];
+                result_image[x + y * result_image_stride] = buffer[validValues / 2];
             } else if (validValues == 1) {
-                result_image[rx + ry * result_image_stride] = buffer[0];
+                result_image[x + y * result_image_stride] = buffer[0];
             } else {
-                result_image[rx + ry * result_image_stride] = 0;
+                result_image[x + y * result_image_stride] = 0;
             }
         } else {
-            result_image[rx + ry * result_image_stride] = 0;
+            result_image[x + y * result_image_stride] = 0;
         }
     }
 
@@ -118,6 +105,7 @@ __global__ void medianFilterMaskedKernel(const float* image, int image_stride, i
 std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilter(const std::shared_ptr<cv::Mat>& image) {
     // Copy the result back to the CPU
     cv::Mat result = cv::Mat(image->rows, image->cols, image->type());
+    cv::copyMakeBorder(*image, *image, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
 
     // Error objects
     cudaError_t err;
@@ -133,36 +121,31 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilter(const std::s
     if(double(image->total()) * image->elemSize() * 2.1 > double(freeMem)) {
         numberOfChunks = fmax(1, pow(4.0, ceil(log(image->total() * image->elemSize() * 2.1 / double(freeMem)) / log(4))));
     }
-    uint chunksPerDim = fmax(1, numberOfChunks/2);
+    uint chunksPerDim = fmax(1, sqrtf(numberOfChunks));
 
     float* deviceImage, *deviceResult;
     uint xMin, xMax, yMin, yMax;
     ulong nSrcStep, nResStep;
     // Apply median filter
-    // Set size where median filter will be applied
-    int2 roi;
-    // Median kernel
-    int2 anchor = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
     // Calculate offsets for image and result. Starting at the edge would result in errors because we would
     // go out of bounds.
-    int2 pSrcOffset = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
-    int2 pResultOffset = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
     dim3 threadsPerBlock, numBlocks;
+
+    int2 realImageDims = {image->cols - 2 * MEDIAN_KERNEL_SIZE, image->rows - 2 * MEDIAN_KERNEL_SIZE};
+    int2 subImageDims;
 
     cv::Mat subImage, subResult, croppedImage;
     for(uint it = 0; it < numberOfChunks; ++it) {
         // Calculate image boarders
-        xMin = (it % chunksPerDim) * image->cols / chunksPerDim;
-        xMax = fmin((it % chunksPerDim + 1) * image->cols / chunksPerDim, image->cols);
-        yMin = (it / chunksPerDim) * image->rows / chunksPerDim;
-        yMax = fmin((it / chunksPerDim + 1) * image->rows / chunksPerDim, image->rows);
+        xMin = (it % chunksPerDim) * realImageDims.x / chunksPerDim;
+        xMax = fmin((it % chunksPerDim + 1) * realImageDims.x / chunksPerDim, realImageDims.x);
+        yMin = (it / chunksPerDim) * realImageDims.y / chunksPerDim;
+        yMax = fmin((it / chunksPerDim + 1) * realImageDims.y / chunksPerDim, realImageDims.y);
 
-        croppedImage = cv::Mat(*image, cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin));
+        croppedImage = cv::Mat(*image, cv::Rect(xMin, yMin, xMax - xMin + 2 * MEDIAN_KERNEL_SIZE, yMax - yMin + 2 * MEDIAN_KERNEL_SIZE));
         croppedImage.copyTo(subImage);
         croppedImage = cv::Mat(result, cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin));
         croppedImage.copyTo(subResult);
-
-        cv::copyMakeBorder(subImage, subImage, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
         cv::copyMakeBorder(subResult, subResult, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
 
         err = cudaMalloc((void **) &deviceImage, subImage.total() * subImage.elemSize());
@@ -193,13 +176,13 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilter(const std::s
 
         // Apply median filter
         auto start = std::chrono::high_resolution_clock::now();
-        roi = {subImage.cols - 2 * MEDIAN_KERNEL_SIZE, subImage.rows - 2 * MEDIAN_KERNEL_SIZE};
+        subImageDims = {subImage.cols, subImage.rows};
         threadsPerBlock = dim3(CUDA_KERNEL_NUM_THREADS, CUDA_KERNEL_NUM_THREADS);
-        numBlocks = dim3(roi.x / threadsPerBlock.x, roi.y / threadsPerBlock.y);
+        numBlocks = dim3(subImageDims.x / threadsPerBlock.x, subImageDims.y / threadsPerBlock.y);
         // Run median filter
-        medianFilterKernel<<<numBlocks, threadsPerBlock>>>(deviceImage, nSrcStep, pSrcOffset,
-                                                           deviceResult, nResStep, pResultOffset,
-                                                           roi, anchor);
+        medianFilterKernel<<<numBlocks, threadsPerBlock>>>(deviceImage, nSrcStep,
+                                                           deviceResult, nResStep,
+                                                           subImageDims);
         cudaDeviceSynchronize();
         auto finish = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = finish - start;
@@ -214,19 +197,23 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilter(const std::s
 
         // Free reserved memory
         cudaFree(deviceImage);
-        cudaFree(deviceResult);
+        cudaFree(deviceResult);     
 
         cv::Rect srcRect = cv::Rect(MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, subResult.cols - 2*MEDIAN_KERNEL_SIZE, subResult.rows - 2*MEDIAN_KERNEL_SIZE);
         cv::Rect dstRect = cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin);
 
         subResult(srcRect).copyTo(result(dstRect));
     }
+    croppedImage = cv::Mat(*image, cv::Rect(MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, image->cols - 2*MEDIAN_KERNEL_SIZE, image->rows - 2*MEDIAN_KERNEL_SIZE));
+    croppedImage.copyTo(*image);
     return std::make_shared<cv::Mat>(result);
 }
 
 std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilterMasked(const std::shared_ptr<cv::Mat>& image, const std::shared_ptr<cv::Mat>& mask) {
     // Copy the result back to the CPU
     cv::Mat result = cv::Mat(image->rows, image->cols, image->type());
+    cv::copyMakeBorder(*image, *image, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
+    cv::copyMakeBorder(*mask, *mask, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
 
     // Error objects
     cudaError_t err;
@@ -242,42 +229,35 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilterMasked(const 
     if(double(image->total()) * image->elemSize() * 3.1 > double(freeMem)) {
         numberOfChunks = fmax(1, pow(4.0, ceil(log(image->total() * image->elemSize() * 3.1 / double(freeMem)) / log(4))));
     }
-    uint chunksPerDim = fmax(1, numberOfChunks/2);
+    uint chunksPerDim = fmax(1, sqrtf(numberOfChunks));
 
     float* deviceImage, *deviceResult;
     uchar* deviceMask;
     uint xMin, xMax, yMin, yMax;
     ulong nSrcStep, nMaskStep, nResStep;
     // Apply median filter
-    // Set size where median filter will be applied
-    int2 roi;
-    // Median kernel
-    int2 anchor = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
     // Calculate offsets for image and result. Starting at the edge would result in errors because we would
     // go out of bounds.
-    int2 pSrcOffset = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
-    int2 pResultOffset = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
-    int2 pMaskOffset = {MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE};
     dim3 threadsPerBlock, numBlocks;
+
+    int2 realImageDims = {image->cols - 2 * MEDIAN_KERNEL_SIZE, image->rows - 2 * MEDIAN_KERNEL_SIZE};
+    int2 subImageDims;
 
     cv::Mat subImage, subMask, subResult, croppedImage;
     for(uint it = 0; it < numberOfChunks; ++it) {
         // Calculate image boarders
-        xMin = (it % chunksPerDim) * image->cols / chunksPerDim;
-        xMax = fmin((it % chunksPerDim + 1) * image->cols / chunksPerDim, image->cols);
-        yMin = (it / chunksPerDim) * image->rows / chunksPerDim;
-        yMax = fmin((it / chunksPerDim + 1) * image->rows / chunksPerDim, image->rows);
+        xMin = (it % chunksPerDim) * realImageDims.x / chunksPerDim;
+        xMax = fmin((it % chunksPerDim + 1) * realImageDims.x / chunksPerDim, realImageDims.x);
+        yMin = (it / chunksPerDim) * realImageDims.y / chunksPerDim;
+        yMax = fmin((it / chunksPerDim + 1) * realImageDims.y / chunksPerDim, realImageDims.y);
 
-        croppedImage = cv::Mat(*image, cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin));
+        croppedImage = cv::Mat(*image, cv::Rect(xMin, yMin, xMax - xMin + 2 * MEDIAN_KERNEL_SIZE, yMax - yMin + 2 * MEDIAN_KERNEL_SIZE));
         croppedImage.copyTo(subImage);
-        croppedImage = cv::Mat(*mask, cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin));
+        croppedImage = cv::Mat(*mask, cv::Rect(xMin, yMin, xMax - xMin + 2 * MEDIAN_KERNEL_SIZE, yMax - yMin + 2 * MEDIAN_KERNEL_SIZE));
         croppedImage.copyTo(subMask);
         croppedImage = cv::Mat(result, cv::Rect(xMin, yMin, xMax - xMin, yMax - yMin));
         croppedImage.copyTo(subResult);
-
-        cv::copyMakeBorder(subImage, subImage, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
         cv::copyMakeBorder(subResult, subResult, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
-        cv::copyMakeBorder(subMask, subMask, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, cv::BORDER_REPLICATE);
 
         err = cudaMalloc((void **) &deviceImage, subImage.total() * subImage.elemSize());
         if (err != cudaSuccess) {
@@ -322,14 +302,14 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilterMasked(const 
         }
 
         // Apply median filter
-        roi = {subImage.cols - 2 * MEDIAN_KERNEL_SIZE, subImage.rows - 2 * MEDIAN_KERNEL_SIZE};
+        subImageDims = {subImage.cols, subImage.rows};
         threadsPerBlock = dim3(CUDA_KERNEL_NUM_THREADS, CUDA_KERNEL_NUM_THREADS);
-        numBlocks = dim3(roi.x / threadsPerBlock.x, roi.y / threadsPerBlock.y);
+        numBlocks = dim3(subImageDims.x / threadsPerBlock.x, subImageDims.y / threadsPerBlock.y);
         // Run median filter
-        medianFilterMaskedKernel<<<numBlocks, threadsPerBlock>>>(deviceImage, nSrcStep, pSrcOffset,
-                                                                 deviceResult, nResStep, pResultOffset,
-                                                                 deviceMask, nMaskStep, pMaskOffset,
-                                                                 roi, anchor);
+        medianFilterMaskedKernel<<<numBlocks, threadsPerBlock>>>(deviceImage, nSrcStep,
+                                                                 deviceResult, nResStep,
+                                                                 deviceMask, nMaskStep,
+                                                                 subImageDims);
 
         err = cudaMemcpy(subResult.data, deviceResult, subImage.total() * subImage.elemSize(), cudaMemcpyDeviceToHost);
         if (err != cudaSuccess) {
@@ -348,6 +328,10 @@ std::shared_ptr<cv::Mat> PLImg::cuda::filters::callCUDAmedianFilterMasked(const 
 
         subResult(srcRect).copyTo(result(dstRect));
     }
+    croppedImage = cv::Mat(*image, cv::Rect(MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, image->cols - 2*MEDIAN_KERNEL_SIZE, image->rows - 2*MEDIAN_KERNEL_SIZE));
+    croppedImage.copyTo(*image);
+    croppedImage = cv::Mat(*mask, cv::Rect(MEDIAN_KERNEL_SIZE, MEDIAN_KERNEL_SIZE, mask->cols - 2*MEDIAN_KERNEL_SIZE, mask->rows - 2*MEDIAN_KERNEL_SIZE));
+    croppedImage.copyTo(*mask);
     return std::make_shared<cv::Mat>(result);
 }
 
