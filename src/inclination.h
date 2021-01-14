f231bc3b0548956ca3fceef7cedb1e1389da2645
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-01-14 11:02:36 +0100

    Added doxygen for inclination
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- src/inclination.h
+++ src/inclination.h
@@ -75,42 +75,76 @@ namespace PLImg {
                            sharedMat grayMask);
 
         /**
+         * Get the im value
          * @return Manually set im value or the mean value in the transmittance based on the highest retardation values
          */
         float im();
         /**
+         * Get the ic value
          * @return Manually set ic value or the maximum value in the gray matter of the transmittance
          */
         float ic();
         /**
+         * Get the rmaxGray value
          * @return Manually set rmaxGray value or point of maximum curvature in the gray matter of the retardation
          */
         float rmaxGray();
         /**
+         * Get the rmaxWhite value
          * @return Manually set rmaxWhite value or the mean value in the retardation based on the highest retardation values
          */
         float rmaxWhite();
 
         /**
+         * Set the im value manually. This will reset the current inclination as it is probably different.
          * @param im Manually set im value
          */
         void set_im(float im);
         /**
+         * Set the ic value manually. This will reset the current inclination as it is probably different.
          * @param ic Manually set ic value
          */
         void set_ic(float ic);
         /**
+         * Set the rmaxGray value manually. This will reset the current inclination as it is probably different.
          * @param rmaxGray Manually set rmaxGray value
          */
         void set_rmaxGray(float rmaxGray);
         /**
+         * Set the rmaxWhite value manually. This will reset the current inclination as it is probably different.
          * @param rmaxWhite Manually set rmaxWhite value
          */
         void set_rmaxWhite(float rmaxWhite);
 
         /**
-         * @brief inclination
-         * @return
+         * This method will compute the inclination values based on the given parameter maps
+         * through Inclination(sharedMat transmittance, sharedMat retardation, sharedMat blurredMask, sharedMat whiteMask, sharedMat grayMask)
+         * or setModalities(sharedMat transmittance, sharedMat retardation, sharedMat blurredMask, sharedMat whiteMask, sharedMat grayMask).
+         * The inclination will be computed using the four values im(), ic(), rmaxWhite() and rmaxGray(). The white and gray matter both use
+         * different formulas. Regions inbetween the white and gray matter are defined through the blurredMask and use a linear interpolation
+         * of both formulas.
+         * The formula is constructed like this \f[
+         * I_{x,y} = \cos^{-1} \sqrt{
+         *                          blurredMask_{x,y} \cdot
+         *                          \left(
+         *                              \frac
+         *                                  {\sin^{-1} r_{x,y}}
+         *                                  {\sin^{-1} rmaxWhite}
+         *                              \cdot
+         *                              \frac
+         *                                  {\log ic/im}
+         *                                  {\log ic/tra_{x,y}}
+         *                          \right) +
+         *                          (1 - blurredMask_{x,y})
+         *                          \cdot
+         *                         \frac
+         *                          {\sin^{-1} r_{x,y}}
+         *                          {\sin^{-1} rmaxGray}
+         *                      }
+         * \f]
+         * Invalid values for the \f$\cos^{-1}\f$ are correted so that they will return either 0 or 1.
+         * The calculated inclination values are finally converted to degree.
+         * @return Shared pointer of a OpenCV matrix containing the inclination values for each pixel in degree.
          */
         sharedMat inclination();
         /**
