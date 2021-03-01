50dd57761ced0c4a125e79332e98f917671db5c4
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-03-01 15:26:54 +0100

    Fix for tests
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- src/version.cpp
+++ src/version.cpp
@@ -5,6 +5,6 @@ const std::string PLImg::Version::PROJECT_VER = "1.1.0";
 const std::string PLImg::Version::PROJECT_VER_MAJOR = "1";
 const std::string PLImg::Version::PROJECT_VER_MINOR = "1";
 const std::string PLImg::Version::PROJECT_VER_PATCH = "0";
-const std::string PLImg::Version::PROJECT_VER_HASH = "e8e9b3f+";
+const std::string PLImg::Version::PROJECT_VER_HASH = "ad5cbfb+";
 const std::string PLImg::Version::PROJECT_VER_BRANCH = "median-filter-investigation";
 const std::string PLImg::Version::PROJECT_VER_TAG = "";
